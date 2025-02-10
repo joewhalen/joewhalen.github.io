207ebae36b6f2bc80b5bd1e5893fae1df8273c6a
Joe Whalen <jwhalen.physics@gmail.com>
2025-02-10 18:38:34 -0500

    added timezone offset

--- static/decimal.js
+++ static/decimal.js
@@ -1,15 +1,17 @@
 function display_c(){
-    var refresh=100000/86400; // Refresh rate in milli seconds
+    var refresh=1; // Refresh rate in milli seconds
     mytime=setTimeout('display_ct()',refresh)
 }
 
 function display_ct() {
     var x = new Date();
-    ms = Date.now()
+    ms_offset = x.getTimezoneOffset() * 60 * 1000;
+    ms = Date.now() - ms_offset;
     s = ((ms/1000) / 86400) % 1 * 86400; // seconds since start of new day
-    console.log(s);
     ds = s * 100000/86400;
-    document.getElementById('ct').innerHTML = "The current Decimal Time is " + Math.floor((ds/100/100)) + ":" + Math.floor(((ds/100)%100)) + ":" + Math.floor(ds%100%100);
-    document.getElementById('ct_title').innerHTML = "Decimal Time - " + ds;
+    timeString = Math.floor((ds/100/100)) + ":" +
+         Math.floor(((ds/100)%100)).toString().padStart(2,'0') + ":" + Math.floor(ds%100%100).toString().padStart(2,'0');
+    document.getElementById('ct_title').innerHTML = "Decimal Time - " + timeString;
+    document.getElementById('ct').innerHTML = "The current Decimal Time is " + timeString;
     display_c();
  }
