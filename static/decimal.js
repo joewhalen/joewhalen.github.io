307f20cc7aebb8036d992f793608b21e0df3cccc
Joe Whalen <jwhalen.physics@gmail.com>
2025-02-10 15:50:34 -0500

    added decimal time beta

---
+++ static/decimal.js
@@ -0,0 +1,14 @@
+function display_c(){
+    var refresh=10; // Refresh rate in milli seconds
+    mytime=setTimeout('display_ct()',refresh)
+}
+
+function display_ct() {
+    var x = new Date();
+    ms = Date.now()
+    s = (ms/1000) % 86400 // seconds since start of new day
+    ds = s * 100000/86400
+    document.getElementById('ct').innerHTML = "The current Decimal Time is " + Math.floor((ds/100/100)) + ":" + Math.floor((ds/100%100)) + ":" + Math.floor(ds%100%100);
+    document.getElementById('ct_title').innerHTML = "Decimal Time - " + MJD;
+    display_c();
+ }
